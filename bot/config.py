a1d25adc13f38bf6f184a9804b6057ca24ce2ffd
vk1719676-byte <vk1719676@gmail.com>
2026-01-03 17:31:58 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,27 +2,27 @@ import os
 
 class Config:
 
-    BOT_TOKEN = ""
+    BOT_TOKEN = "8437147669:AAHt-TI_xCtVPO6v_IiQNKaiU0zySUsH_wQ"
 
-    SESSION_NAME = ""
+    SESSION_NAME = "yt_upload_stream_bot"
 
-    API_ID = ""
+    API_ID = "20989027"
 
-    API_HASH = ""
+    API_HASH = "199a12271f5490b90d321ce2ef9cdd12"
 
-    CLIENT_ID = ""
+    CLIENT_ID = "770993770266-jo5d0kh4v9gb3srej6c6p9huqj7176bf.apps.googleusercontent.com"
 
-    CLIENT_SECRET = ""
+    CLIENT_SECRET = "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"
 
-    AUTH_USERS = [942731625]
+    AUTH_USERS = [2069782530]
 
     VIDEO_DESCRIPTION = ""
 
     VIDEO_CATEGORY = ""
 
-    VIDEO_TITLE_PREFIX = ""
+    VIDEO_TITLE_PREFIX = "Hi"
 
-    VIDEO_TITLE_SUFFIX = ""
+    VIDEO_TITLE_SUFFIX = "Last"
     
     DEBUG = bool()
 
